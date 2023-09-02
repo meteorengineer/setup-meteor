86c4f3aa864799d27c390b24540a7685d558f521
Michał Powaga <michalpowaga13@gmail.com>
2023-09-02 15:14:00 +0000

    Update release.sh

--- release.sh
+++ release.sh
@@ -4,9 +4,9 @@ npm ci
 NODE_OPTIONS=--openssl-legacy-provider npm run build
 git add dist
 git commit -m "npm run build"
-git push origin master
 
 VERSION=$(npm version patch)
 git push origin $VERSION
 git tag --force v1
 git push --force origin v1
+git push origin master
